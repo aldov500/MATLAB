5b1aca96db648de96d28bb8f5f0482783b4e43aa
Aldo Vargas <aldo.vargas.meza94@gmail.com>
2017-12-12 16:23:55 -0600

    Mat update

--- Proyecto_Protocolos/main.m
+++ Proyecto_Protocolos/main.m
@@ -9,14 +9,27 @@ word = 'Ingenierias'
 outMatrix = [];
 outVector = [];
 asciiWord = double(word)
-asciiWordNoised = asciiWord + (rand(1,length(asciiWord)))
-asciiWordRounded = round(asciiWordNoised)
+%asciiWordNoised = asciiWord + (rand(1,length(asciiWord)))
+%asciiWordRounded = round(asciiWordNoised)
+outMatrixOriginal = [];
 
 for(index = 1:1:length(word))
+    % binary convertion
     vector = de2bi(asciiWord(index));
+    outMatrixOriginal = [outMatrixOriginal; vector];
+    
+    % get parity
     vectorPar = getVRC(vector);
+    
+    % concatenate
     outVector = [outVector, vectorPar];
     outMatrix = [outMatrix; vectorPar];
 end
 
-crcOut = getCRC(outVector)
+outMatrixOriginal
+outMatrix = getLRC(outMatrix)
+outVector
+
+crcResult = getCRC(outVector);
+dataCRC = [outVector, crcResult];
+crcComp = getCRC(dataCRC);
